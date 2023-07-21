97fc10006d24b86c51da0f2ab0d3c6ea0efaf2fd
Muhammad Mubashir <mmubashirmemon@gmail.com>
2023-07-21 22:11:20 +0500

    Added

---
+++ app/src/main/java/com/example/login_app/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.login_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
